3a05798f74a2e8a8706209fd46de00320ac5f4dd
Rafael T. C. Soares <rafaelcba@gmail.com>
2024-10-01 00:07:35 +0000

    updating code samples

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
   std::cout << "Hello, World!\n";
+  std::cout << "Number of arguments: " << argc << std::endl;
+
+  for (int i = 0; i < argc; ++i) {
+      std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+  }
   return 0;
 }
